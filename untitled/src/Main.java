73fb274f6aa2c5bd8d827878d6d0ccac03479231
Eduardo Toural <eduardotoural@Eduardos-MacBook-Pro.local>
2016-07-26 21:19:21 -0400

    Initial commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by eduardotoural on 7/21/16.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        Person person = new Person("Eduardo", "Toural", 29);
+
+        System.out.println("Hello World");
+
+
+
+
+    }
+
+}
